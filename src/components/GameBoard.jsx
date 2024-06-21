758e8d4d8996548f9837bd127c9b17edb5d45965
ezbuckeye <105604551+ezbuckeye@users.noreply.github.com>
2024-06-20 19:59:52 -0500

    implemented gameboard layout

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,23 @@
+const gameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
